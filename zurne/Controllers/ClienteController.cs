e4d01f35025eaaed05dac8d79098a7060e8cd6e6
lidia-freitas <lydiaoliveira@gmail.com>
2017-03-31 22:02:37 -0300

    Inclusao dos metodos nos controllers

---
+++ zurne/Controllers/ClienteController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace Controllers
+{
+    class ClienteController
+    {
+
+        private static List<Cliente> listaClientes = new List<Cliente>();
+
+        private Cliente Buscar(int id)
+        {
+            foreach (Cliente cli in listaClientes)
+            {
+                if (cli.ID == id)
+                {
+                    return cli;
+                }
+            }
+
+            return null;
+        }
+
+        public List<Cliente> Listar()
+        {
+            return listaClientes;
+        }
+
+        public void CadastrarPf(string nome, string cpf, string email, string endereco)
+        {
+            PessoaFisica pf = new PessoaFisica (nome, cpf, email, endereco);
+
+            Cliente cli = new Cliente(pf);
+
+            listaClientes.Add(cli);
+        }
+        
+        public void CadastrarPj(string rz, string cnpj, string contato, string email, string endereco)
+        {
+            PessoaJuridica pj = new PessoaJuridica (rz, cnpj, contato, email, endereco);
+
+            Cliente cli = new Cliente(pj);
+
+            listaClientes.Add(cli);
+        }
+
+        public void Remover(int id)
+        {
+            foreach (Cliente cli in listaClientes)
+            {
+                if (cli.ID == id)
+                {
+                    listaClientes.Remove(cli);
+                }
+            }
+        }
+
+        public void EditarPf(int id, string nome, string cpf, string email, string end)
+        {
+            Cliente cli = Buscar(id);
+
+            if (cli != null)
+            {
+                cli.PessoaFisica.Nome = nome;
+                cli.PessoaFisica.CPF = cpf;
+                cli.PessoaFisica.Email = email;
+                cli.PessoaFisica.Endereco = end;
+            }
+        }
+        
+        public void EditarPj(int id, string rz, string cnpj, string contato, string email, string endereco)
+        {
+            Cliente cli = Buscar(id);
+
+            if (cli != null)
+            {
+                cli.PessoaJuridica.RazaoSocial = rz;
+                cli.PessoaJuridica.CNPJ = cnpj;
+                cli.PessoaJuridica.Email = email;
+                cli.PessoaJuridica.Endereco = endereco;
+                cli.PessoaJuridica.Contato = contato;
+            }
+        }
+
+    }
+}
---
+++ zurne/Controllers/FuncionarioController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace Controllers
+{
+    public class FuncionarioController
+    {
+        private static List<Funcionario> listaFuncionarios = new List<Funcionario>();
+
+        private Funcionario Buscar(int id) {
+            foreach (Funcionario func in listaFuncionarios)
+            {
+                if (func.ID == id)
+                {
+                    return func;
+                }
+            }
+
+            return null;
+        }
+
+        public List<Funcionario> Listar()
+        {
+            return listaFuncionarios;
+        }
+
+        public void Cadastrar(string nome, string cpf, string email, string end)
+        {
+            Funcionario func = new Funcionario(nome, cpf, email, end);
+
+            listaFuncionarios.Add(func);
+        }
+
+        public void Remover(int id)
+        {
+            foreach (Funcionario func in listaFuncionarios)
+            {
+                if(func.ID == id)
+                {
+                    listaFuncionarios.Remove(func);
+                }
+            }
+        }
+
+        public void Editar(int id, string nome, string cpf, string email, string end)
+        {
+            Funcionario func = Buscar(id);
+
+            if(func != null)
+            {
+                func.PessoaFisica.Nome = nome;
+                func.PessoaFisica.CPF = cpf;
+                func.PessoaFisica.Email = email;
+                func.PessoaFisica.Endereco = end;
+            }
+        }
+
+    }
+
+
+}
